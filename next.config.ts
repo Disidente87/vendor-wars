476f8d0e445b15d6040cf9bfcec2d43fb52bca88
Disidente87 <criptodisidente@gmail.com>
2025-08-05 18:07:41 -0700

    Fix next.config.ts: Remove deprecated instrumentationHook option for Next.js 15

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
+  
+  // Disable performance profiling to prevent .cpuprofile files
+  experimental: {
+    // instrumentationHook is no longer needed in Next.js 15
+  },
+  
+  // Disable webpack performance profiling
+  webpack: (config, { isServer, dev }) => {
     // Exclude figma folder from webpack compilation
     config.watchOptions = {
       ...config.watchOptions,
@@ -15,7 +22,28 @@ const nextConfig: NextConfig = {
       loader: 'ignore-loader'
     });
     
+    // Disable performance profiling in development
+    if (dev) {
+      config.optimization = {
+        ...config.optimization,
+        removeAvailableModules: false,
+        removeEmptyChunks: false,
+        splitChunks: false,
+      };
+      
+      // Disable source maps for better performance
+      config.devtool = false;
+    }
+    
     return config;
+  },
+  
+  // Disable performance monitoring
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
   },
 };
 
