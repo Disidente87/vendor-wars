1da903da788ee28efc96a0e438a5aac9107f4783
Disidente87 <criptodisidente@gmail.com>
2025-08-07 14:18:19 -0700

    wagmiprovider config fixed

--- src/components/MiniAppWrapper.tsx
+++ src/components/MiniAppWrapper.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import { MiniAppProvider } from '@neynar/react'
+import dynamic from 'next/dynamic'
+
+// Dynamically import WagmiProvider to avoid SSR issues
+const WagmiProvider = dynamic(
+  () => import('@/components/providers/WagmiProvider'),
+  { ssr: false }
+)
 
 export function MiniAppWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <MiniAppProvider analyticsEnabled={true}>
-      {children}
-    </MiniAppProvider>
+    <WagmiProvider>
+      <MiniAppProvider analyticsEnabled={true}>
+        {children}
+      </MiniAppProvider>
+    </WagmiProvider>
   )
 } 
